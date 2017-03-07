cf2f1818d82b47b1156e3ae0c914e8c5a01331ae
bryanhmiller <bryankalos@hotmail.com>
2017-03-06 19:58:42 -0600

    Created an xhr in main.js, also added 2 functions to show it working

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log("it's linked");
 
+function executeThisCodeAfterFileLoaded(){
+	console.log("It Worked!!!!");
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("booooo");
+}
+
+
+
+
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
